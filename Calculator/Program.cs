e59a73bbc6e2a463f15064f78d0451db229e9ff3
Aoife Cross <Cross-A3@ulster.ac.uk>
2025-09-29 02:43:26 +0100

    implamented program code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Ask the user to imput the numbers
+Console.WriteLine("type the first number and hit enter");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type the second number and hit enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Preform the calculation
+int result = firstNumber + secondNumber;
+
+// Output answer to console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+
+
+
+
